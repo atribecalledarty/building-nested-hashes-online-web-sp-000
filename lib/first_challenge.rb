dea994af75efad5632cb96987476e48b454b0614
Arthur Song <arth3rs0ng@gmail.com>
2019-04-05 17:37:15 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,17 +15,46 @@ def first_challenge
          status: "Alive"
        },
        hero_friends: [
-          {
-            name: "Benvolio",
-            age: "17",
-            attitude: "worried"
-          },
-          {
-            name: "Mercutio",
-            age: "18",
-            attitude: "hot-headed"
-          }
-        ]
-     }
-   }
+        {
+          name: "Benvolio",
+          age: "17",
+          attitude: "worried"
+        },
+        {
+          name: "Mercutio",
+          age: "18",
+          attitude: "hot-headed"
+        }
+      ]
+    },
+    capulet: {
+      patriarch: {
+        name: "Lord Capulet",
+        age: "50"
+      },
+      matriarch: {
+        name: "Lady Capulet",
+        age: "51"
+      },
+      heroine: {
+        name: "Juliet",
+        age: "15",
+        status: "Alive"
+      },
+      heroine_friends: 
+      [
+        {
+          name: "Steven",
+          age: "30",
+          attitude: "confused"
+        },
+        {
+          name: "Nurse",
+          age: "44",
+          attitude: "worried"
+        }
+      ]
+      
+    }
+  }
 end

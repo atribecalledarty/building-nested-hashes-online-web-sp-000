ff925e5123764e9f527fd12943753da981e24813
Arthur Song <arth3rs0ng@gmail.com>
2019-04-05 17:33:32 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,31 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     montagues: {
+       patriarch: {
+         name: "Lord Montague",
+         age: "53"
+       },
+       matriarch: {
+         name: "Lady Montague",
+         age: "54"
+       },
+       hero: {
+         name: "Romeo",
+         age: "15",
+         status: "Alive"
+       },
+       hero_friends: [
+          {
+            name: "Benvolio",
+            age: "17",
+            attitude: "worried"
+          },
+          {
+            name: "Mercutio",
+            age: "18",
+            attitude: "hot-headed"
+          }
+        ]
+     }
+   }
 end
